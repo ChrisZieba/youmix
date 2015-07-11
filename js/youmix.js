45c8536387a2605199f6fe76c5a0521ae8418aac
Chris Zieba <zieba.chris@gmail.com>
2015-07-10 22:23:54 -0700

    Add dependencies

--- js/youmix.js
+++ js/youmix.js
@@ -1,1 +1,8 @@
-console.log('test');
+$(document).ready(function() {
+	var content = '<div class="ytp-button ytp-drop-down-menu-button" role="option" aria-labelledby="ytp-menu-speed" aria-checked="false" tabindex="2300"><div class="ytp-drop-down-menu-button-check"></div>0.75</div>';
+	
+	// Get the speed list for the HTML5 player
+	var menuSpeed = $("#ytp-menu-speed");
+
+	menuSpeed.next(".ytp-menu-cell").find(".ytp-drop-down-menu-content").append(content);
+});
