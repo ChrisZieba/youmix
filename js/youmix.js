1695f8b3d598e0f0bc2dc1b56404b77b381ffe9c
Chris Zieba <zieba.chris@gmail.com>
2015-07-12 22:48:42 -0700

    Add html block

--- js/youmix.js
+++ js/youmix.js
@@ -6,3 +6,14 @@ $(document).ready(function() {
 
 	menuSpeed.next(".ytp-menu-cell").find(".ytp-drop-down-menu-content").append(content);
 });
+
+
+<div class="ytp-menu-row">
+	<div class="ytp-menu-cell ytp-menu-title" id="ytp-menu-audio-mode">Mode</div>
+	<div class="ytp-menu-cell">
+		<div class="ytp-segmented-control" role="radiogroup" aria-label="Autoplay">
+			<div class="ytp-button ytp-segmented-control-deselected" role="radio" aria-labelledby="ytp-menu-autoplay" aria-checked="false" tabindex="2050">Off</div>
+			<div class="ytp-button ytp-segmented-control-other ytp-segmented-control-selected" role="radio" aria-labelledby="ytp-menu-autoplay" aria-checked="true" tabindex="2050">On</div>
+		</div>
+	</div>
+</div>
