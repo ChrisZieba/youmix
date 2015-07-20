3a2b142152c080b80805a6ca39cf8c0562b8f745
Chris Zieba <zieba.chris@gmail.com>
2015-07-19 23:30:20 -0700

    Add the settings for the compressor option

--- src/youmix.js
+++ src/youmix.js
@@ -53,6 +53,17 @@ $(document).ready(function() {
 
       case 'lowpass':
 
+      case 'compressor'
+        // Connect the source node to the destination
+        source.connect(compressor);
+        compressor.threshold.value = -50;
+        compressor.knee.value = 40;
+        compressor.ratio.value = 12;
+        compressor.reduction.value = -20;
+        compressor.attack.value = 0;
+        compressor.release.value = 0.25;
+      break;
+
       case 'hallway':
         
         source.connect(biquadFilter);
